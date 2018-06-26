b669368f518d4d30fa8ce2e37ff3b62a95beda22
ttk-781 <31058811+ttk-781@users.noreply.github.com>
2018-06-26 21:27:45 +0530

    2nd Commit

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -3,7 +3,7 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hellow World");
+System.out.println("Hellow by Dear");
 		
 	}
 
