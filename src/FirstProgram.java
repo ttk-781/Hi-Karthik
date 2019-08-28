024da006c2eb5756abeb6a7f2933b6eccb17e1bd
ttk-781 <31058811+ttk-781@users.noreply.github.com>
2019-08-28 15:37:31 +0530

    Update FirstProgram.java

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -4,7 +4,7 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hellow by Dear");
-		Wait
+		Wait for the lunch
 		
 	}
 
